0d350ec27a3c53adf1c984dabaf0000057d435db
Pol Piella Abadia <hi@polpiella.dev>
2023-10-05 17:05:37 +0100

    Splits logic into sub command

---
+++ Musicli/Commands/Search.swift
@@ -0,0 +1,34 @@
+import ArgumentParser
+import MusicKit
+
+struct Search: AsyncParsableCommand {
+    func run() async throws {
+        guard await isAppAuthorised() else { return }
+        
+        print("Enter the name of the song you'd like to search for...")
+        guard let query = readLine() else { return }
+        print("")
+        let request = MusicCatalogSearchRequest(term: query, types: [Song.self])
+        let searchResponse = try await request.response()
+        
+        print("Search results for: '\(query)'")
+        print("")
+        
+        for song in searchResponse.songs {
+            print("Title: " + song.title)
+            print("Artist: " + song.artistName)
+            print("Album: " + song.albumTitle!)
+            print("Genres: " + song.genreNames.joined(separator: ", "))
+            print("")
+        }
+    }
+    
+    private func isAppAuthorised() async -> Bool {
+        guard MusicAuthorization.currentStatus != .authorized else {
+            return true
+        }
+        
+        let response = await MusicAuthorization.request()
+        return response == .authorized
+    }
+}
--- Musicli/Musicli.swift
+++ Musicli/Musicli.swift
@@ -1,36 +1,9 @@
-import MusicKit
 import ArgumentParser
-import Foundation
 
 @main
 struct Musicli: AsyncParsableCommand {
-    func run() async throws {
-        guard await isAppAuthorised() else { return }
-        
-        print("Enter the name of the song you'd like to search for...")
-        guard let query = readLine() else { return }
-        print("")
-        let request = MusicCatalogSearchRequest(term: query, types: [Song.self])
-        let searchResponse = try await request.response()
-        
-        print("Search results for: '\(query)'")
-        print("")
-        
-        for song in searchResponse.songs {
-            print("Title: " + song.title)
-            print("Artist: " + song.artistName)
-            print("Album: " + song.albumTitle!)
-            print("Genres: " + song.genreNames.joined(separator: ", "))
-            print("")
-        }
-    }
-    
-    private func isAppAuthorised() async -> Bool {
-        guard MusicAuthorization.currentStatus != .authorized else {
-            return true
-        }
-        
-        let response = await MusicAuthorization.request()
-        return response == .authorized
-    }
+    static var configuration = CommandConfiguration(
+        subcommands: [Search.self],
+        defaultSubcommand: Search.self
+    )
 }
